05a19fef08032222d524fd33196e8484c8bf93d3
RokasV <vezikauskas.r@gmail.com>
2024-01-23 08:47:47 +0200

    global style

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,9 +1,11 @@
+import '../styles/button.css'
+
 export default function AboutPage() {
   return (
     <div>
       <h1>AboutPage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ad, qui ab ex magnam accusamus nemo similique harum, minima porro natus cupiditate exercitationem praesentium saepe ullam, inventore non rem eligendi. Quo perspiciatis inventore necessitatibus quos deleniti, fugiat accusamus iure consectetur, quis tempore quisquam quae eligendi obcaecati asperiores veritatis a sed.</p>
-      <button>Read more</button>
+      <button className='btn'>Read more</button>
     </div>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ export default function HomePage() {
     <div>
       <h1>HomePage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ad, qui ab ex magnam accusamus nemo similique harum, minima porro natus cupiditate exercitationem praesentium saepe ullam, inventore non rem eligendi. Quo perspiciatis inventore necessitatibus quos deleniti, fugiat accusamus iure consectetur, quis tempore quisquam quae eligendi obcaecati asperiores veritatis a sed.</p>
-      <button>Read more</button>
+      <button className="btn">Read more</button>
     </div>
   )
 }
